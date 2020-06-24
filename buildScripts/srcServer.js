3c443d1cd644f4b4449d053caaabaf21a4bc21d1
Josef Bnm <ssefbnm@gmail.com>
2020-06-24 22:16:09 +0100

    add travis CI

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -3,9 +3,10 @@ import path from "path"
 // import open from "open" //to open our site in browser
 import webpack from 'webpack'
 import config from '../webpack.config.dev'
+// "start:dev": "webpack-dev-server --mode development --open",
 
 /* eslint-disable no-console */
-const port = 8888
+const port = 8080
 const app = express()
 const compiler = webpack(config)
 
@@ -16,6 +17,14 @@ app.use(require('webpack-dev-middleware')(compiler, {
 // tell express which route to handle
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html')) // "__dirname" give us the directory we running in
+})
+
+app.get('/users', function(req,res) {
+    res.json([
+        {"id":1,"firstName":"Josef","lastName":"Bnm","email":"ssefbnm@gmail.com"},
+        {"id":2,"firstName":"Mohamed","lastName":"Didi","email":"mohamin@hotmail.fr"},
+        {"id":3,"firstName":"Fatima-Zahra","lastName":"Bnm","email":"fatbnm@gmail.com"}
+    ])
 })
 
 app.listen(port, function(err) {
